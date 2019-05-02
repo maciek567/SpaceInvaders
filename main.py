f5c09fd998392ac91decbd78758022d555775057
MaciejBanaś <maciejb.98@gmail.com>
2019-05-02 14:15:35 +0200

    blinking after ship is shot

--- main.py
+++ main.py
@@ -40,14 +40,16 @@ class Player(object):
 class Enemy(object):
     alien = pygame.image.load('img/alien.png')
 
-    def __init__(self, x, y, width, height, end):
+    def __init__(self, x, y, width, height, start, end, status):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
+        self.start = start
         self.end = end
-        self.path = [self.x, self.end]  # where our enemy starts and finishes his path.
+        self.path = [self.start, self.end]  # where our enemy starts and finishes his path.
         self.vel = 3
+        self.status = True  # death or alive
 
     def draw(self, win):
         self.move()
@@ -59,11 +61,13 @@ class Enemy(object):
             if self.x + self.vel < self.path[1]:
                 self.x += self.vel
             else:
+                self.y += 40
                 self.vel = self.vel * -1
         else:
             if self.x - self.vel > self.path[0]:
                 self.x += self.vel
             else:
+                self.y += 40
                 self.vel = self.vel * -1
 
     # when hit : invaderKilled.play()
@@ -75,24 +79,53 @@ class Projectile(object):
         self.y = y
         self.radius = radius
         self.color = color
-        self.vel = 8  # velocity of bullet
+        self.vel = 6  # velocity of bullet
 
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
 
+def draw_block_of_enemies():
+    x = 20
+    y = 20
+    for j in range(len(enemy)):
+        for i in range(len(enemy[j])):
+            enemy[j][i] = Enemy(x, y, 60, 60, i*60, screenWidth - 60*(10-i), True)
+            x += 60
+        x = 20
+        y += 40
+        i = 1
+
+    return enemy
+
+# for every foe check collision with projectiles on screen
+def check_collison(foe):
+    for projectile in projectiles:
+        if  foe.x <= projectile.x <= foe.x + 60 and foe.status is True:
+            if foe.y <= projectile.y <= foe.y + 40:
+                projectiles.pop(projectiles.index(projectile))
+                return False
+    return True
+
 # in every frame display all objects on their current positions
 def redraw_game_window():
     win.blit(bg, (0, 0))
     player.draw(win)
-    enemy.draw(win)
 
     for bullet in projectiles:
         bullet.draw(win)
 
+    for i in range(len(enemy)):
+        for j in range(len(enemy[i])):
+            if check_collison(enemy[i][j]) is True and enemy[i][j].status is True:
+                enemy[i][j].draw(win)
+            else:
+                enemy[i][j].status = False
+
     pygame.display.update()
 
 
+<<<<<<< HEAD
 def menu():
     intro = True
     while intro:
@@ -106,19 +139,18 @@ def menu():
 
 screenWidth = 800
 screenHeight = 480
+=======
+screenWidth = 1280
+screenHeight = 800
+>>>>>>> 244097c826a6626bb45f5b83f6470d646fc92f1f
 win = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption("Space Invaders")
 clock = pygame.time.Clock()
 
 bg = pygame.image.load('img/sky.jpg')
-music = pygame.mixer.music.load('sounds/music1.wav')
-pygame.mixer.music.play(-1)
-shoot = pygame.mixer.Sound('sounds/shoot.wav')
-invaderKilled = pygame.mixer.Sound('sounds/invaderKilled.wav')
-explosion = pygame.mixer.Sound('sounds/explosion.wav')
-
 player = Player(20, screenHeight - 80, 60, 60)
-enemy = Enemy(20, 20, 60, 60, screenWidth - 80)
+enemy = [[None]*10, [None]*10, [None]*10]
+enemy = draw_block_of_enemies()
 projectiles = []
 
 # Ship can shoot only if this variable is equal to 0. After every successful shoot this variable is incremented,
@@ -153,8 +185,12 @@ while run:
         player.x -= player.vel
     elif keys[pygame.K_RIGHT] and player.x < screenWidth - player.width - player.vel and not player.killed:
         player.x += player.vel
+<<<<<<< HEAD
     if keys[pygame.K_SPACE] and canShoot == 0 and not player.killed:
         shoot.play()
+=======
+    if keys[pygame.K_SPACE] and canShoot == 0:
+>>>>>>> 244097c826a6626bb45f5b83f6470d646fc92f1f
         if len(projectiles) < 10:  # up to 10 projectiles on screen at the same moment
             projectiles.append(Projectile(round(player.x + player.width // 2),
                                           round(player.y + player.height // 2), 4, (255, 128, 0)))
@@ -165,4 +201,5 @@ while run:
 
     redraw_game_window()
 
+#End of game!
 pygame.quit()
